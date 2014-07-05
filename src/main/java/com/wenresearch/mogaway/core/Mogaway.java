44a093f8a1af4bf84f796fa1304b41c27285f2b9
Ali Irawan <boylevantz@gmail.com>
2014-07-05 23:38:11 +0700

    Mogaway object

---
+++ src/main/java/com/wenresearch/mogaway/core/Mogaway.java
@@ -0,0 +1,20 @@
+package com.wenresearch.mogaway.core;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.wenresearch.mogaway.http.Server;
+
+@Component
+@Scope("prototype")
+public class Mogaway {
+
+	@Autowired
+	private Server server;
+
+	public Server getServer() {
+		return server;
+	}
+
+}
