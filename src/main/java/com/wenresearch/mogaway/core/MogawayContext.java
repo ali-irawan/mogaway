9403f794655c8007e020f33966036c2e7d5e2b5c
Ali Irawan <boylevantz@gmail.com>
2014-07-05 23:37:56 +0700

    adding application context aware

---
+++ src/main/java/com/wenresearch/mogaway/core/MogawayContext.java
@@ -0,0 +1,22 @@
+package com.wenresearch.mogaway.core;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MogawayContext implements ApplicationContextAware {
+
+	private static ApplicationContext context;
+
+	public static ApplicationContext getContext() {
+		return context;
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		MogawayContext.context = context;
+	}
+	
+}
