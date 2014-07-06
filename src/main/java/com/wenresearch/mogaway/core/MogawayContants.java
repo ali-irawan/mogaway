dbd808cf9f4e93af3a3f3eca148303895801fb37
Ali Irawan <boylevantz@gmail.com>
2014-07-06 10:32:58 +0700

    adding Class for storing Constants

---
+++ src/main/java/com/wenresearch/mogaway/core/MogawayContants.java
@@ -0,0 +1,12 @@
+package com.wenresearch.mogaway.core;
+
+public class MogawayContants {
+
+	public static final String REQUEST_METHOD_GET = "GET";
+	public static final String REQUEST_METHOD_POST = "POST";
+	
+	public static final String PARAM_URL = "url";
+	public static final String PARAM_METHOD = "method";
+	public static final String PARAM_CONTENT_TYPE = "contentType";
+	public static final String PARAM_DATA = "data";
+}
